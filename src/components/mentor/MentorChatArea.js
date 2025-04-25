12ece76baeee27fd02cbb8f23448ef2e65b775b9
esuyom <sylee0560@gmail.com>
2025-04-25 11:01:39 +0900

    # 멘토챗 수정

--- src/components/mentor/MentorChatArea.js
+++ src/components/mentor/MentorChatArea.js
@@ -1,18 +1,35 @@
 import React from "react";
+import { useRef, useEffect } from "react";
 
 function MentorChatArea({ chatList }) {
+  const chatAreaRef = useRef(null);
+
+  useEffect(() => {
+    // 새 메시지 생기면 맨 아래로 스크롤
+    if (chatAreaRef.current) {
+      chatAreaRef.current.scrollTop = 0;
+    }
+  }, [chatList]);
+
   return (
-    <div className="chataArea">
-      {chatList.map((chat, idx) => (
-        <div
-          key={idx}
-          className={`bubble ${
-            chat.type === "mentor" ? "bubbleLeft" : "bubbleRight"
-          }`}
-        >
-          <span>{chat.message}</span>
-        </div>
-      ))}
+    <div className="chatArea" ref={chatAreaRef}>
+      <div className="spacer"></div>
+      <div className="chatList">
+        {chatList.map((chat, idx) => (
+          <div
+            key={idx}
+            className={`bubble ${
+              chat.type === "mentor" ? "bubbleLeft" : "bubbleRight"
+            }`}
+          >
+            <span>{chat.message}</span>
+          </div>
+        ))}
+      </div>
+      {/* <div className="directInput">
+        <input type="text" d/>
+        <button type="submit">전송</button>
+      </div> */}
     </div>
   );
 }
